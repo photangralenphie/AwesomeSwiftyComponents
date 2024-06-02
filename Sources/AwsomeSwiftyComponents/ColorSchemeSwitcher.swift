0ace22a859455adea1fc3fa3888db0dfe7d3ffac
Jonas Helmer <64361476+photangralenphie@users.noreply.github.com>
2024-06-02 11:45:17 +0200

    Added ColorSchemeSwitcher, CreditManager, InlineColorPicker

---
+++ Sources/AwsomeSwiftyComponents/ColorSchemeSwitcher.swift
@@ -0,0 +1,71 @@
+import SwiftUI
+
+@available(iOS 14.0.0, *)
+public struct ColorSchemeSwitcher: View {
+    
+    @Binding public var colorScheme: PreferredColorScheme
+    @State public var showIcon: Bool
+    
+    public init(colorScheme: Binding<PreferredColorScheme>, showIcon: Bool) {
+        _colorScheme = colorScheme
+        self.showIcon = showIcon
+    }
+    
+    public var body: some View {
+        HStack {
+            Image(systemName: colorScheme.icon())
+                .foregroundStyle(Color.accentColor)
+                .modifier(iOS17Stuff(feedbackTrigger: colorScheme))
+            Picker("Is Dark?", selection: $colorScheme) {
+                Text("System")
+                    .tag(PreferredColorScheme.systemDefault)
+                Text("Dark")
+                    .tag(PreferredColorScheme.dark)
+                Text("Light")
+                    .tag(PreferredColorScheme.light)
+            }
+            .pickerStyle(.segmented)
+        }
+    }
+}
+
+fileprivate struct iOS17Stuff : ViewModifier {
+    let feedbackTrigger: PreferredColorScheme
+    func body(content: Content) -> some View {
+        if #available(iOS 17.0, *) {
+            content
+                .contentTransition(.symbolEffect(.replace))
+                .sensoryFeedback(.selection, trigger: feedbackTrigger)
+        } else {
+            content
+        }
+    }
+}
+
+public enum PreferredColorScheme: String, CaseIterable {
+    case light
+    case dark
+    case systemDefault
+    
+    fileprivate func icon() -> String {
+        switch self {
+            case .dark:
+                return "moon.circle"
+            case .light:
+                return "sun.max.circle"
+            case .systemDefault:
+                return "circle.lefthalf.filled"
+        }
+    }
+    
+    public func mode() -> ColorScheme? {
+        switch self {
+        case .dark:
+            return .dark
+        case .light:
+            return .light
+        case .systemDefault:
+            return nil
+        }
+    }
+}
---
+++ Sources/AwsomeSwiftyComponents/InlineColorPicker.swift
@@ -0,0 +1,74 @@
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct InlineColorPicker: View {
+    
+    @Binding public var colorIndex: Int
+    
+    @State public var pickerStyle: ColorPickerStyle
+    @State public var systemImage: String
+    @State public var description: String
+    
+    public init(colorIndex: Binding<Int>, pickerStyle: ColorPickerStyle, systemImage: String = "paintbrush", description: String = "Accent Color:") {
+        _colorIndex = colorIndex
+        self.pickerStyle = pickerStyle
+        self.systemImage = systemImage
+        self.description = description
+    }
+    
+    public var body: some View {
+        VStack {
+            if pickerStyle == .expanded {
+                HStack {
+                    Label(description, systemImage: systemImage)
+                    Spacer()
+                    Text(GetColorByID(colorIndex).description.capitalized)
+                        .foregroundColor(GetColorByID(colorIndex))
+                }
+            }
+
+            HStack {
+                ForEach(availibleColors, id: \.self) { color in
+                    Circle()
+                        .strokeBorder(GetColorByID(colorIndex) == color ? Color.gray : color , lineWidth: 3)
+                        .background {
+                            if (color == Color.primary) {
+                                Image(systemName: "circle.righthalf.fill")
+                                    .imageScale(.large)
+                            } else {
+                                Circle().foregroundColor(color)
+                            }
+                        }
+                        .padding(.horizontal, -13)
+                        .onTapGesture {
+                            colorIndex = availibleColors.firstIndex(of: color)!
+                        }
+                }
+            }
+        }
+        .padding(.vertical, 7)
+        .modifier(Feedback(colorIndex: colorIndex))
+    }
+}
+
+fileprivate struct Feedback: ViewModifier {
+    let colorIndex: Int
+    func body(content: Content) -> some View {
+        if #available(iOS 17.0, *) {
+            content
+                .sensoryFeedback(.selection, trigger: colorIndex)
+        } else {
+            content
+        }
+    }
+}
+
+public enum ColorPickerStyle {
+    case slim, expanded
+}
+
+fileprivate let availibleColors = [Color.blue, Color.cyan, Color.mint, Color.green, Color.yellow, Color.orange, Color.red, Color.purple, Color.indigo, Color.primary]
+
+public func GetColorByID(_ ID: Int) -> Color {
+    return availibleColors[ID]
+}
