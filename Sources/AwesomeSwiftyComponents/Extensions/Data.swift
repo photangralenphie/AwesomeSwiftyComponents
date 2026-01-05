62c339ec54e4a289eceda9f19824fa7b137cdef8
Jonas Helmer <64361476+photangralenphie@users.noreply.github.com>
2026-01-05 13:41:28 +0100

    add extension making Data Identifiable

---
+++ Sources/AwesomeSwiftyComponents/Extensions/Data.swift
@@ -0,0 +1,12 @@
+//
+//  Data.swift
+//  AwesomeSwiftyComponents
+//
+//  Created by Jonas Helmer on 04.01.26.
+//
+
+import Foundation
+
+extension Data: Identifiable {
+	public var id: Data { self }
+}
