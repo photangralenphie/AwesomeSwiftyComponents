6452dc5439b3fabcdec83ee11b5eadf3aa31d770
Jonas Helmer <64361476+photangralenphie@users.noreply.github.com>
2026-01-10 15:55:27 +0100

    update to TintedEmoji and Emoji Label to use characters adds Color to TinitedEmoji init

--- Sources/AwesomeSwiftyComponents/TintedEmoji.swift
+++ Sources/AwesomeSwiftyComponents/TintedEmoji.swift
@@ -7,41 +7,50 @@
 
 import SwiftUI
 
-/// Creates a tinted emoji in the set `SwiftUI/Color/accentColor`.
+/// Creates a tinted emoji.
 /// Works best with emojis with one or few colors.
+///
+/// Example:
+/// ```swift
+/// TintedEmoji("🔥", color: .red)
+/// ```
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
 public struct TintedEmoji: View {
 	
-	private let emoji: String
+	private let emoji: Text
+	private let color: Color
 	
-	/// Creates a tinted emoji in the set `SwiftUI/Color/accentColor`.
+	/// Creates a tinted emoji.
 	/// Works best with emojis with one or few colors.
 	/// - Parameter emoji: The emoji to use,
-	public init(_ emoji: String) {
-		self.emoji = emoji
+	/// - Parameter color: The color to use (default: Color.accentColor).
+	public init(_ emoji: Character, color: Color = .accentColor) {
+		self.emoji = Text(String(emoji))
+		self.color = color
 	}
 	
     public var body: some View {
-		Text(emoji)
+		emoji
 			.opacity(0)
-			.overlay{
-				Color.accentColor
-					.mask(Text(emoji))
+			.overlay {
+				color.mask(emoji)
 			}
     }
 }
 
 @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, visionOS 1.0, *)
 public extension Label where Title == Text, Icon == TintedEmoji {
-	/// Creates a `SwiftUI/Label` from a title and an emoji
+	/// Creates a ``SwiftUI/Label`` from a title and an emoji
 	/// - Parameters:
 	///   - title: The title of the label.
 	///   - emoji: The emoji of the label used as icon.
-	init(_ title: LocalizedStringKey, emoji: String) {
-		self.init {
-			Text(title)
-		} icon: {
-			TintedEmoji(emoji)
-		}
+	///
+	/// Example:
+	/// ```swift
+	/// Label("Fire", emoji: "🔥")
+	/// ```
+	init(_ title: LocalizedStringKey, emoji: Character) {
+		self.init { Text(title) } icon: { TintedEmoji(emoji) }
 	}
 }
+
